080bab383e5b7070ffe0ffa41f21ca42625f5825
Dylan Dreyer <dyldreyer04@gmail.com>
2017-10-09 13:39:35 -0700

    removing garbage pm_set_lowest signature

--- drivers/periph_common/pm.c
+++ drivers/periph_common/pm.c
@@ -24,7 +24,7 @@
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
-void __attribute__((weak)) pm_set_lowest(void) {}
+//void __attribute__((weak)) pm_set_lowest(void) {}
 
 void __attribute__((weak)) pm_off(void)
 {
